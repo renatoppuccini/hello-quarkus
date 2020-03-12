e34142b4488557aa25b0a9a8914849f3df71b449
renatoppuccini@gmail.com <renatoppuccini@gmail.com>
2020-03-12 15:40:59 -0400

    upload files

---
+++ src/main/java/org/acme/ExampleResource.java
@@ -0,0 +1,31 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Path("/hello")
+public class ExampleResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+
+        InetAddress ip = null;
+        String hostname = null;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+ 
+        } catch (UnknownHostException e) {
+ 
+            e.printStackTrace();
+        }
+        return "hello: " + ip ;
+    }
+}
